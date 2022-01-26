fb9e59a0b5f0643a0d736ba8b6c3807f8e4ce61e
Joyce Mary Thomas <78149448+joycethomas-123@users.noreply.github.com>
2022-01-26 18:45:45 +0530

    Create Oddeven.java

---
+++ Oddeven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Oddeven
+{
+	public static void main(String[] args) {
+	Scanner obj = new scanner(System.in);
+	int n = obj.nextInt();
+	if(n%2==0)
+	{
+	    Syatem.out.println("The no. is even");
+	}
+	else{
+	    	System.out.println("The no. is odd");
+	}
+	}
+}
