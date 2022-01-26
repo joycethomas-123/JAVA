1b23a640d749888ba3ccd4a82950363f5c9d4a04
Joyce Mary Thomas <78149448+joycethomas-123@users.noreply.github.com>
2022-01-26 18:44:02 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		int a = obj.nextInt();
+		int b = obj.nextInt();
+		int sum;
+		sum = a+b;
+		System.out.println("Sum = "+sum);
+	}
+}
