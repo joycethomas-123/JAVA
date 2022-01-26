c8975db8100a6b7f83be1d30e0befded908a4d25
Joyce Mary Thomas <78149448+joycethomas-123@users.noreply.github.com>
2022-01-26 20:41:12 +0530

    Create SumElements.java

---
+++ Lab 2/SumElements.java
@@ -0,0 +1,19 @@
+i
+}mport java.util.*;
+class SumElements
+{
+  public static void main(String args[])
+  {
+    int i,sum=0;
+    System.out.println("Enter the number of elements present in the array");
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int a[]=new int[n];
+    System.out.println("Sum of the elements is: ");
+    for(i=0;i<n;i++)
+    {
+      sum=sum+a[i];
+    }
+    System.out.println(+sum);
+   }
+   }
